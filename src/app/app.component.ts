22484aef8a8b9ac03e058d3f58d7456d6da2167a
fredy.schlag <fredy.schlag@senior.com.br>
2018-11-14 11:27:12 -0200

    Obtendo dados do usuário autenticado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { PlatformClient, UsuariosService } from "./app.service";
+import { DadosUsuario } from "./classes"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styles: [],
+  providers: [
+    PlatformClient, UsuariosService
+  ]
 })
+
 export class AppComponent {
   title = 'tutorial-aplicacao-integrada-plataforma';
+  dadosUsuario = new DadosUsuario();
+
+  constructor(private usuarios: UsuariosService) { }
+
+  ngOnInit() {
+    this.getDadosUsuario();
+  }
+
+  //Busca os dados do usuário autenticado
+  getDadosUsuario() {
+    this.usuarios.getDadosUsuario().subscribe(data => {
+      this.dadosUsuario = data;
+    });
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { service, user } from '@seniorsistemas/senior-platform-data';
+import { Observable, forkJoin, from } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+
+@Injectable()
+export class PlatformClient {
+    constructor(private http: HttpClient) { }
+
+    getPlatformAPI(api: string): Observable<any> {
+        return forkJoin(
+            from(service.getRestUrl()),
+            from(user.getAuthHeader()),
+        ).pipe(mergeMap((values: any) => {
+            const [restUrl, authHeader] = values;
+            let headers = new HttpHeaders({
+                "Authorization": authHeader
+            });
+            return this.http.get<any>(`${restUrl}` + api, { headers });
+        }));
+    }
+}
+
+@Injectable()
+export class UsuariosService {
+    constructor(private client: PlatformClient) { }
+
+    getDadosUsuario(): Observable<any> {
+        return this.client.getPlatformAPI('usuarios/userManager/queries/obterMeusDados');
+    }
+}
